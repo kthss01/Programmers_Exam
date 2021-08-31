04c62302ebd5ccbeb3e6e38cb807072b3f0031b9
kthss01 <kthss01@naver.com>
2021-09-01 08:45:28 +0900

    이미지 상세보기 닫기 구현

--- FrontEnd/Cat_Pitcture_Search/src/ImageInfo.js
+++ FrontEnd/Cat_Pitcture_Search/src/ImageInfo.js
@@ -5,6 +5,7 @@ class ImageInfo {
     constructor({ $target, data }) {
       const $imageInfo = document.createElement("div");
       $imageInfo.className = "ImageInfo";
+      // $imageInfo.classList.add('test');
       this.$imageInfo = $imageInfo;
       $target.appendChild($imageInfo);
   
@@ -35,9 +36,31 @@ class ImageInfo {
             </div>
           </div>`;
         this.$imageInfo.style.display = "block";
+  
+        this.$imageInfo.querySelector('.close').addEventListener("click", () => {
+            this.close();
+        });
+        window.addEventListener('keyup', e => {
+            if (e.key === 27) {
+              this.close();
+            }
+        });
+        this.$imageInfo.addEventListener("click", (e) => {
+          // console.log(e.target);
+          // console.log(e.target.getAttribute('class'));
+  
+          if (e.target.getAttribute('class') === 'ImageInfo') {
+            this.close();
+          }
+        });
       } else {
         this.$imageInfo.style.display = "none";
       }
+    }
+  
+    close() {
+        this.data.visible = false;
+        this.render();
     }
   }
   
