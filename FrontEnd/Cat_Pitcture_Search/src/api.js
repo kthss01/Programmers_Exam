8a11b9c78c7e56a86c6d7f261396d5ba0d02eef9
kthss01 <kthss01@naver.com>
2021-09-01 10:20:23 +0900

    async, await로 변경

--- FrontEnd/Cat_Pitcture_Search/src/api.js
+++ FrontEnd/Cat_Pitcture_Search/src/api.js
@@ -1,16 +1,21 @@
 const API_ENDPOINT =
   "https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev";
 
+const request = async (url) => {     
+  try {       
+    const result = await fetch(url);       
+    return result.json();     
+  } catch (e) {       
+    console.warn(e);     
+  }   
+}  
+
 const api = {
   fetchCats: keyword => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then(res =>
-      res.json()
-    );
+    return request(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
   },
 
   fetchCat: id => {
-    return fetch(`${API_ENDPOINT}/api/cats/${id}`).then(res =>
-      res.json()
-    );
+    return request(`${API_ENDPOINT}/api/cats/${id}`);
   }
 };
