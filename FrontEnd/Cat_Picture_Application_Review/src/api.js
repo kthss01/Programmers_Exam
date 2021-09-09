a9c1fe08674d1933fd11af0d8b86f5f5bf73a752
kthss01 <kthss01@naver.com>
2021-09-09 09:25:07 +0900

    fetch 함수로 데이터 불러오기 api.js

---
+++ FrontEnd/Cat_Picture_Application_Review/src/api.js
@@ -0,0 +1,20 @@
+// api.js
+
+// api end point를 상수처리 해두면 나중에 변경 되었을 겨우 처리하기 쉬움
+const API_ENDPOINT = 
+    "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev";
+
+const request = (nodeId) => {
+    // nodeId 유무에 따라 root directory를 조회할지 특정 directory를 조회할지 처리
+    fetch(`${API_ENDPOINT}/${nodeId ? nodeId : ''}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('서버의 상태가 이상합니다');
+            }
+
+            return response.json();
+        })
+        .catch((e) => {
+            throw new Error(`무언가 잘못 되었습니다! ${e.message}`);
+        });
+}
