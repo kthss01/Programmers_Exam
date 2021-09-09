aac4a637e03d03ca3646f13561525a45a99d55b7
kthss01 <kthss01@naver.com>
2021-09-09 09:26:30 +0900

    async ~ await 사용하기

--- FrontEnd/Cat_Picture_Application_Review/src/api.js
+++ FrontEnd/Cat_Picture_Application_Review/src/api.js
@@ -6,15 +6,15 @@ const API_ENDPOINT =
 
 const request = (nodeId) => {
     // nodeId 유무에 따라 root directory를 조회할지 특정 directory를 조회할지 처리
-    fetch(`${API_ENDPOINT}/${nodeId ? nodeId : ''}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('서버의 상태가 이상합니다');
-            }
+    try {
+        const res = await fetch(`${API_ENDPOINT}/${nodeId ? nodeId : ''}`);
 
-            return response.json();
-        })
-        .catch((e) => {
-            throw new Error(`무언가 잘못 되었습니다! ${e.message}`);
-        });
+        if (!res.ok) {
+            throw new Error('서버의 상태가 이상합니다!');
+        }
+
+        return await res.json();
+    } catch(e) {
+        throw new Error(`무언가 잘못 되었습니다! ${e.message}`);
+    }
 }
